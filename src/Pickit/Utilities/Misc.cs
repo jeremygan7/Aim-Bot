43a09fd404bd459eab76b8eb52c5a8f529aea792
lurumdare <8020186+lurumdare@users.noreply.github.com>
2020-08-08 06:32:01 +0300

    Update Misc.cs

--- src/Pickit/Utilities/Misc.cs
+++ src/Pickit/Utilities/Misc.cs
@@ -5,40 +5,50 @@ using SharpDX;
 
 namespace AimBot.Utilities
 {
-    public class Misc
+    public static class Misc
     {
-        //public static int EntityDistance(EntityWrapper entity)
-        //{
-        //    var Object = entity.GetComponent<Render>();
-        //    return (int) Math.Sqrt(Math.Pow(Player.X - Object.X, 2) + Math.Pow(Player.Y - Object.Y, 2));
-        //}
-
-        public static int EntityDistance(Entity entity)
-        {
-            var object_ = entity.GetComponent<Render>();
-            return Convert.ToInt32(Vector3.Distance(Player.entity_.Pos, object_.Pos));
-        }
-        public static float EntityDistance(Entity entity, Entity player)
+        public static int EntityDistance(Entity entity, Entity player)
         {
             var component = entity?.GetComponent<Render>();
 
             if (component == null)
-                return 9999999f;
+                return 9999999;
 
             var objectPosition = component.Pos;
 
-            return Vector3.Distance(objectPosition, player.GetComponent<Render>().Pos);
+            return Convert.ToInt32(Vector3.Distance(objectPosition, player.GetComponent<Render>().Pos));
         }
+
+        //public static int EntityDistance(EntityWrapper entity)
+
+        //{
+
+        //    var Object = entity.GetComponent<Render>();
+
+        //    return (int) Math.Sqrt(Math.Pow(Player.X - Object.X, 2) + Math.Pow(Player.Y - Object.Y, 2));
+
+        //}
+
+
+        //public static int EntityDistance(Entity entity)
+
+        //{
+
+        //    var object_ = entity.GetComponent<Render>();
+
+        //    return Convert.ToInt32(Vector3.Distance(Player.entity_.Pos, object_.Pos));
+
+        //}
         //public static int EntityDistance(Entity entity)
         //{
         //    var Object = entity.GetComponent<Render>();
         //    return (int)Math.Sqrt(Math.Pow(Player.X - Object.X, 2) + Math.Pow(Player.Y - Object.Y, 2));
         //}
 
-        public static int GetEntityDistance(Vector2 firstPos, Vector2 secondPos)
-        {
-            var distanceToEntity = Math.Sqrt(Math.Pow(firstPos.X - secondPos.X, 2) + Math.Pow(firstPos.Y - secondPos.Y, 2));
-            return (int) distanceToEntity;
-        }
+        //public static int GetEntityDistance(Vector2 firstPos, Vector2 secondPos)
+        //{
+        //    var distanceToEntity = Math.Sqrt(Math.Pow(firstPos.X - secondPos.X, 2) + Math.Pow(firstPos.Y - secondPos.Y, 2));
+        //    return (int) distanceToEntity;
+        //}
     }
 }
