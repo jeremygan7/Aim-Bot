6e00e6e0277c451d598017134e8f3142347b6f36
IlliumIv <alm.sparta.ao@gmail.com>
2020-08-20 21:30:56 +0500

    Fix references.

---
+++ Utilities/Keyboard.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AimBot.Utilities
+{
+    public static class Keyboard
+    {
+        private const int KeyeventfExtendedkey = 0x0001;
+        private const int KeyeventfKeyup       = 0x0002;
+
+        private const int ActionDelay = 1;
+
+        [DllImport("user32.dll")]
+        private static extern uint Keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+
+        public static void KeyDown(Keys key) { Keybd_event((byte) key, 0, KeyeventfExtendedkey | 0, 0); }
+
+        public static void KeyUp(Keys key)
+        {
+            Keybd_event((byte) key, 0, KeyeventfExtendedkey | KeyeventfKeyup, 0); //0x7F
+        }
+
+        public static void KeyPress(Keys key)
+        {
+
+            KeyDown(key);
+            Thread.Sleep(ActionDelay);
+            KeyUp(key);
+        }
+
+        [DllImport("USER32.dll")]
+        private static extern short GetKeyState(int nVirtKey);
+
+        public static bool IsKeyDown(int nVirtKey) => GetKeyState(nVirtKey) < 0;
+    }
+}
---
+++ Utilities/Player.cs
@@ -0,0 +1,11 @@
+﻿using ExileCore;
+using ExileCore.PoEMemory.Components;
+using ExileCore.PoEMemory.MemoryObjects;
+
+namespace AimBot.Utilities
+{
+    public static class Player
+    {
+        public static Entity entity_ { get; }
+    }
+}
